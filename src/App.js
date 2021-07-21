22b38a1298ab9bb8ca1392a1ad5b130b696b0ba1
rahulprasad2796 <78995259+rahulprasad2796@users.noreply.github.com>
2021-07-21 17:55:01 +0530

    o

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Card from './components/cards';
+import "./App.css";
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      products: [],
+      loader: true,
+      filterProducts: [],
+    }
+  }
+
+  //this code works fine instead of handleSearch
+  //inline code 
+  // (e) => {
+  //   const filter = this.state.filterProducts.filter(v => v.Title.toLowerCase().includes(e.target.value));
+  //   this.setState({products: filter});
+  // }
+
+  handleSearch() {
+    //shows error TypeError: Cannot read property 'state' of undefined
+    //Why this.state is not accessible outside render, but in your code when you explained
+    //it was accessible, am I doing something wrong?
+    console.log(this.state.products);
+  }
+
+
+  render() {
+    return (
+      <div>
+        {this.state.loader ? (<h1>Loading...</h1>) : (<div>
+          <input type="text" placeholder="Search"  onChange={this.handleSearch}>
+        </input>
+        <div className="card-wrapper">
+          {this.state.products.map((v, i) => <Card {...v} key={i} />)}
+        </div>
+        </div>) }
+      </div>
+    )
+  }
+
+  componentDidMount() {
+    axios.get("https://www.omdbapi.com/?apikey=45f0782a&s=war")
+    .then(res => {
+      this.setState({products: res.data.Search, filterProducts: res.data.Search, loader:false});
+    })
+  }
 }
 
 export default App;
